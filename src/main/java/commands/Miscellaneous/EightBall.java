ccf4d85033c44782011b8e1b3d3665f3b3f981ca
Andy Zhao <andyzhao21@gmail.com>
2022-07-13 22:28:24 -0400

    EightBall command

--- src/main/java/commands/Miscellaneous/EightBall.java
+++ src/main/java/commands/Miscellaneous/EightBall.java
@@ -3,6 +3,7 @@ package commands.Miscellaneous;
 import commandutils.Command;
 import commandutils.CommandContext;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -19,9 +20,17 @@ public class EightBall implements Command {
 
     @Override
     public void onCommand(CommandContext context) {
-        List<String> responses = List.of("Without a Doubt", "It is Certain", "Signs Point to Yes", "As I see it, Yes",
-                "Very Doubtful", "Ask Again Later", "Better Not Tell You Now", "Concentrate and Ask Again",
-                "Reply Hazy Try Again", "Don't Count On It");
+        ArrayList<String> responses = new ArrayList<>();
+        responses.add("Without a Doubt");
+        responses.add("It is Certain");
+        responses.add("Signs Point to Yes");
+        responses.add("As I see it, Yes");
+        responses.add("Very Doubtful");
+        responses.add("Ask Again Later");
+        responses.add("Better Not Tell You Now");
+        responses.add("Concentrate and Ask Again");
+        responses.add("Reply Hazy Try Again");
+        responses.add("Don't Count On It");
         Random rand = new Random();
         int getResponse = rand.nextInt(responses.size());
         context.getChannel().sendMessage(responses.get(getResponse)).queue();
